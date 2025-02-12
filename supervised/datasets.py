c4359c09158d436f31a9943385bcf67b502e1d88
Md. Ahanaf Arif Khan <ahanaf019@gmail.com>
2025-02-13 00:07:15 +0600

    Implement SiameseModel and ContrastiveLoss

--- supervised/datasets.py
+++ supervised/datasets.py
@@ -20,3 +20,19 @@ class ImageClassificationDataset(Dataset):
         if self.transforms is not None:
             image = self.transforms(image)
         return image, label
+
+
+class SiameseDataset(Dataset):
+    def __init__(self, images, labels, image_size=(96, 96), transforms=None):
+        super().__init__()
+        self.images = images
+        self.labels = labels
+        self.image_size = image_size
+        self.transforms = transforms
+    
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, index):
+        # Incomplete
+        pass
--- supervised/models.py
+++ supervised/models.py
@@ -18,3 +18,21 @@ class CNNModel(nn.Module):
     
     def forward(self, x):
         return self.cnn(x)
+
+
+class SiameseNetwork(nn.Module):
+    def __init__(self, embed_dim, pretrained=True):
+        super().__init__()
+        self.cnn = torchvision.models.resnet50(torchvision.models.ResNet50_Weights.DEFAULT if pretrained else None)
+        self.cnn.fc = nn.Sequential(
+            nn.Dropout(p=0.4),
+            nn.Linear(2048, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(True),
+            nn.Linear(512, embed_dim)
+        )
+    
+    def forward(self, x1, x2):
+        out1 = self.cnn(x1)
+        out2 = self.cnn(x2)
+        return out1, out2
